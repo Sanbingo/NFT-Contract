55e91cf2e2fd131585e160984ef3cbf2e741d564
san <linrisan@qq.com>
2022-11-06 19:36:52 +0800

    feat: 添加部分注释说明

--- nft.sol
+++ nft.sol
@@ -7,9 +7,10 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
+// ERC721Enumerable 与 ERC721A 合约的应用场景：https://www.frank.hk/blog/azuki-erc721a?hmsr=joyk.com&utm_source=joyk.com&utm_medium=referral
 contract SanMeta is ERC721Enumerable, Ownable {
     using Strings for uint256;
-    
+    // 售卖开关、盲盒开关
     bool public _isSaleActive = false;
     bool public _revealed = false;
 
@@ -43,11 +44,12 @@ contract SanMeta is ERC721Enumerable, Ownable {
         for(uint256 i = 0; i < tokenQuantity; i++) {
             uint256 mintIndex = totalSupply();
             if (totalSupply() < MAX_SUPPLY) {
+                // 调用合约铸造NFT，第二个参数确保之前不存在
                 _safeMint(msg.sender, mintIndex);
             }
         }
     }
-
+    // virtual override 重载修饰符（覆盖原来的方法）
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
         if (_revealed == false) {
@@ -61,6 +63,8 @@ contract SanMeta is ERC721Enumerable, Ownable {
         if (bytes(base).length == 0) {
             return _tokenURI;
         }
+
+        // abi.encodePacked对给定参数执行 紧打包编码，返回bytes
 
         // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked)
         if (bytes(_tokenURI).length > 0) {
